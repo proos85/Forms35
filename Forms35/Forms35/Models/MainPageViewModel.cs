65c51f3366f496ecd76ccb55e66408913680b5aa
Patrick Roos <patrick@point42.nl>
2019-02-10 19:51:21 +0100

    Add project files.

---
+++ Forms35/Forms35/Models/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Forms35.Models
+{
+    public class MainPageViewModel: INotifyPropertyChanged
+    {
+        private readonly ContentPage _page;
+        private string _phoneNumber;
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        public ICommand CallCommand { get; set; }
+
+        public MainPageViewModel(ContentPage page)
+        {
+            _page = page;
+            CallCommand = new Command(CallNumber);
+        }
+
+        private async void CallNumber()
+        {
+            try
+            {
+                PhoneDialer.Open(PhoneNumber);
+            }
+            catch (ArgumentNullException ex)
+            {
+                await _page.DisplayAlert("Error", ex.ToString(), "OK");
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                await _page.DisplayAlert("Error", ex.ToString(), "OK");
+            }
+            catch (Exception ex)
+            {
+                await _page.DisplayAlert("Error", ex.ToString(), "OK");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Forms35/Forms35/Pages/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Forms35.Models;
+
+namespace Forms35.Pages
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            var vm = new MainPageViewModel(this)
+            {
+                PhoneNumber = "0682781934"
+            };
+            BindingContext = vm;
+
+            InitializeComponent();
+        }
+    }
+}
